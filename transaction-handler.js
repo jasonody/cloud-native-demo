42260a8e4860c0c37324e2d2e71221803fc4ad30
Jason Ody <jasonody@users.noreply.github.com>
2019-02-03 22:44:03 -0800

    added DynamoDB transaction table and lambda to consume CDC stream

--- transaction-handler.js
+++ transaction-handler.js
@@ -4,11 +4,44 @@ const aws = require('aws-sdk')
 const _ = require('highland')
 const uuid = require('uuid')
 
+const db = new aws.DynamoDB.DocumentClient()
+const kinesis = new aws.Kinesis()
+
 module.exports.command = (event, context, callback) => {
   console.log('event: %j', event)
 
   const item = JSON.parse(event.body)
   item.id = uuid.v4()
+
+  const params = {
+    TableName: process.env.TRANSACTION_TABLE,
+    Item: item,
+  };
+
+  console.log('dynamoDB params: %j', params)
+
+  db.put(params).promise()
+    .then(resp => callback(null, {
+      statusCode: 201,
+      headers: {
+        'access-control-allow-origin': '*', // CORS support
+        'cache-control': 'no-cache',
+      },
+    }))
+    .catch(err => callback(err))
+}
+
+module.exports.publish = (event, context, callback) => {
+  _(event.Records)
+  .tap(r => console.log('record: %j', r))
+  //.map(covertToJSON)
+  //.flatMap(putObject)
+  .collect()
+  .toCallback(callback)
+}
+
+const publishEvent = (record) => {
+  //TODO: CREATE item FROM RECORD
 
   const streamEvent = {
     id: uuid.v1(),
@@ -25,20 +58,7 @@ module.exports.command = (event, context, callback) => {
     Data: new Buffer.from(JSON.stringify(streamEvent)),
   }
 
-  const kinesis = new aws.Kinesis()
-
-  kinesis.putRecord(params).promise()
-    .then(resp => callback(null, {
-      statusCode: 201,
-      headers: {
-        'access-control-allow-origin': '*', // CORS support
-        'cache-control': 'no-cache',
-      },
-    }))
-    .catch(err => callback(err))
-}
-
-module.exports.publish = (event, context, callback) => {
+  return _(kinesis.putRecord(params).promise())
 }
 
 module.exports.subscribe = (event, context, callback) => {
